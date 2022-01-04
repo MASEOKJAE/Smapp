b284aeb46700f52320d4388386026230534ebb83
MASEOKJAE <maasj7514@gmail.com>
2022-01-04 14:05:06 +0900

    Initial Commit

---
+++ Smapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smapp
+//
+//  Created by 마석재 on 2022/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
