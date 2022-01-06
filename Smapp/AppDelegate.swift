703806e7c4d3f59364e39ec0eb962e51cb5f1ec0
MASEOKJAE <maasj7514@gmail.com>
2022-01-06 13:38:56 +0900

    sj 로그인

--- Smapp/AppDelegate.swift
+++ Smapp/AppDelegate.swift
@@ -7,17 +7,31 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         
         return true
+    }
+    
+    // google login handler
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var handled: Bool
+
+          handled = GIDSignIn.sharedInstance.handle(url)
+          if handled {
+            return true
+          }
+
+          // Handle other custom URL types.
+
+          // If not handled by this app, return false.
+          return false
     }
 
     // MARK: UISceneSession Lifecycle
--- Smapp/ViewController.swift
+++ Smapp/ViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
+    @IBOutlet var AppTitle: UILabel!
+    @IBOutlet var AppSubTitle: UILabel!
+    @IBOutlet var GoogleLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,5 +22,22 @@ class ViewController: UIViewController {
     }
 
 
-}
+    @IBAction func TapGoogleLogin(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        let signInConfig = GIDConfiguration.init(clientID: clientID)
+        
+      GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: self) { user, error in
+        guard error == nil else { return }
 
+        guard let authentication = user?.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken!, accessToken: authentication.accessToken)
+        // access token 부여 받음
+        
+        // 파베 인증정보 등록
+        Auth.auth().signIn(with: credential) {_,_ in
+            // token을 넘겨주면, 성공했는지 안했는지에 대한 result값과 error값을 넘겨줌
+            //self.showMainViewController()
+        }
+      }
+    }
+}
