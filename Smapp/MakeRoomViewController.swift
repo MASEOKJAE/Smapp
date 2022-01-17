568f8b0285dba9223a3382ffe75692fb4a45fa53
MASEOKJAE <maasj7514@gmail.com>
2022-01-17 14:31:20 +0900

    EnterRoom

---
+++ Smapp/MakeRoomViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MakeRoomViewController.swift
+//  Smapp
+//
+//  Created by 마석재 on 2022/01/17.
+//
+
+import UIKit
+
+class MakeRoomViewCotroller: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
+    
+    
+    // 스터디 설명 구현란
+    
+    //var ExplainContents: String? = nil
+   
+    @IBOutlet var WriteExplain: UITextField!
+    
+    @IBAction func ExitMake(_ sender: UIButton) {
+    }
+    
+    @IBAction func SaveButton(_ sender: UIButton) {
+    
+    }
+    
+}
