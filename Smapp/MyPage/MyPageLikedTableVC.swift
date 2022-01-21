26e9a3d47faef2110e24e4001aa1a38b9ed76bc0
jiina28 <jiina_28@naver.com>
2022-01-21 15:24:19 +0900

    Move data to chatroom

--- Smapp/MyPage/MyPageLikedTableVC.swift
+++ Smapp/MyPage/MyPageLikedTableVC.swift
@@ -8,31 +8,52 @@
 import UIKit
 
 class MyPageLikedTableVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    let likedTitleOfRoom = ["네똑 스터디 하실 분ㅠㅠ~", "웹서개 스터디 입니다!!"]
-    let likedPartsOfRoom = ["2/4", "4/4"]
-    
+
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        return arr.count
-//    }
+    func initRefresh() {
+        let refresh = UIRefreshControl()
+        refresh.addTarget(self, action: #selector(updateUI(refresh:)), for: .valueChanged)
+        //refresh.attributedTitle = NSAttributedString(string: "새로고침")
+        
+        tableView.addSubview(refresh)
+    }
+    
+    @objc
+    func updateUI(refresh: UIRefreshControl) {
+        refresh.endRefreshing()
+        tableView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.reloadData()
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return likedTitleOfRoom.count
+        return self.appDelegate.roomList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableCell", for: indexPath) as? MyPageTableCell else {
              return UITableViewCell()
          }
-         cell.likedRoomTitle.text = likedTitleOfRoom[indexPath.row]
-         cell.LikedRoomParticipants.text = likedPartsOfRoom[indexPath.row]
+        
+        let item = self.appDelegate.roomList[indexPath.item]
+        
+        cell.likedRoomTitle.text = item.title
+        cell.LikedRoomParticipants.text = String(item.numberOfPart!) + "/" + String(item.numberOfMax!)
+         cell.subject_prof.text = String(item.subject!) + "-" + String(item.professor!)
+        
          return cell
 
 //        return UITableViewCell()
     }
+    
 }
 
