6cd5e2e9aafada6c7dcc102acb99739c17854e54
young170 <seongbin10209@gmail.com>
2022-01-25 11:27:11 +0900

    get name

--- Smapp/Register/RegisterVC.swift
+++ Smapp/Register/RegisterVC.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
-
+import GoogleSignIn
+import Firebase
+import FirebaseDatabase
 
 class RegisterVC: UIViewController {
+    
+    var ref: DatabaseReference!
     
     @IBOutlet weak var allAccept: UIButton!
     
@@ -19,9 +23,22 @@ class RegisterVC: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextField!
     
-
+    @IBOutlet weak var nameSelect: UIButton!
+    
+    @IBAction func nameClicked(_ sender: UIButton) {
+        userName()
+    }
+    
+    func userName() {
+        let refUser = ref.child("userList")
+        
+        refUser.child(String((GIDSignIn.sharedInstance.currentUser?.profile!.email.prefix(8))!)).updateChildValues(["name": nameTextField.text!])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.ref = Database.database(url: "https://smapp-69029-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
     }
     
     //약관 동의 체크 박스
